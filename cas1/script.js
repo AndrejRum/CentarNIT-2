3e362a9c80703dac49861f31a69aaa0c3a544f96
Andrejj03 <andrejj.contact@gmail.com>
2023-05-12 18:10:24 +0200

    <3

---
+++ cas1/script.js
@@ -0,0 +1,12 @@
+// // let a = 2;
+// // let b = 3;
+
+// // console.log(a+b);
+
+
+// let r = [2,2,5,5,7,23,52,234,234];
+// for (let i = 0;i<r.length;i++){
+//   console.log(r[i]);
+// }
+
+console.log('POZDRAV');
