0a26c85328d4a1ce3843b6e207b84f655e79a067
Andrejj03 <andrejj.contact@gmail.com>
2023-05-15 17:12:30 +0200

    <3

--- Shopping_Cart/script.js
+++ Shopping_Cart/script.js
@@ -1,4 +1,14 @@
 function test(){
-  console.log('test');
-  
+ 
+ let link2 = document.querySelector('.drugi_link');
+ let link1 = link2.previousElementSibling; 
+ let link3 = link2.nextElementSibling;
+
+
+
+
+
+ console.log(link1.innerText);
+ console.log(link2.innerText);
+ console.log(link3.innerText);
 }
