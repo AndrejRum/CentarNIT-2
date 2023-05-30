15d3168c9ceb77f1cf2f87d9c0f8526a68938732
Andrejj03 <andrejj.contact@gmail.com>
2023-05-30 18:03:24 +0200

    >/<

--- cas5/script.js
+++ cas5/script.js
@@ -1,44 +1,64 @@
-// function neka_funkcija(br1,br2,operacija){
-//   if(br1 === Number(br1) && br2 === Number(br2)){
-//   switch(operacija){
-//     case '+':
-//       console.log(br1+br2);
-//       break;
-//     case '-':
-//     console.log(br1-br2);
-//     break;
-//     case '*':
-//       console.log(br1*br2);
-//       break;
-//     case '+':
-//         console.log(br1/br2);
-//         break;
-//   default: 
-//     console.log("Uneli ste pogresnu operaciju!")
+// // function neka_funkcija(br1,br2,operacija){
+// //   if(br1 === Number(br1) && br2 === Number(br2)){
+// //   switch(operacija){
+// //     case '+':
+// //       console.log(br1+br2);
+// //       break;
+// //     case '-':
+// //     console.log(br1-br2);
+// //     break;
+// //     case '*':
+// //       console.log(br1*br2);
+// //       break;
+// //     case '+':
+// //         console.log(br1/br2);
+// //         break;
+// //   default: 
+// //     console.log("Uneli ste pogresnu operaciju!")
+// //   }
+// // }else{
+// //   console.log('nesto nije u redu!')
+// // }
+// // }
+// // let br1 = Number(prompt('unesite prvi broj'));
+// // let br2 = Number(prompt('Unesite drugi broj'));
+// // let operacija = prompt('Unesite operaciju');
+
+
+
+// // neka_funkcija(br1, br2, operacija)
+
+
+
+// function probera(ime , prezime){
+//   if(ime === 'Andrej'){
+//     console.log('Pravilno ste uneli ime')
+//   }else{
+//     console.log('Nepravilno uneto ime.')
 //   }
-// }else{
-//   console.log('nesto nije u redu!')
+//   if(prezime === 'Rumenic'){
+//     console.log('Pravilno ste uneli prezime')
+//   }else{
+//     console.log('')
+//   }
 // }
-// }
-// let br1 = Number(prompt('unesite prvi broj'));
-// let br2 = Number(prompt('Unesite drugi broj'));
-// let operacija = prompt('Unesite operaciju');
+
+// /// ako je deljiv sa 2 i 3 
+// // fiz bas
 
 
 
-// neka_funkcija(br1, br2, operacija)
 
-
-
-function probera(ime , prezime){
-  if(ime === 'Andrej'){
-    console.log('Pravilno ste uneli ime')
-  }else{
-    console.log('Nepravilno uneto ime.')
+function provera_deljivosti(broj){
+  if(broj % 3 === 0 && broj % 5 === 0){
+    console.log('FizzBuzz');
   }
-  if(prezime === 'Rumenic'){
-    console.log('Pravilno ste uneli prezime')
+  if(broj % 3 === 0){
+    console.log('Fizz');
+  }else if(broj % 5 === 0){
+    console.log('Buzz');
+
   }else{
-    console.log('')
+    console.log('Broj nije deljiv ni sa 3 ni sa 5 :<');
   }
 }
