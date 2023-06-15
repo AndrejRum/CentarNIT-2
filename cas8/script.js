ef7a5f047773899c9dede75cd997a364f619c207
Andrejj03 <andrejj.contact@gmail.com>
2023-06-15 16:32:18 +0200

    <3

---
+++ cas8/script.js
@@ -0,0 +1,33 @@
+// const nekiNiz = [6, 7, 8,9,81,34];
+
+// let noviNiz = [];
+// let nekiNiz2 = [];
+
+// for(let i = 0; i < nekiNiz.length;i++){
+//  noviNiz.push(nekiNiz[i]*3)
+
+// }
+// console.log(noviNiz);
+
+// for(let i = 0; i < noviNiz.length;i++){
+//   if(noviNiz[i]>20 && noviNiz[i] < 30 ){
+//     nekiNiz2.push(noviNiz[i]*4);
+//   }else if(noviNiz[i]>20){
+//     nekiNiz2.push(((noviNiz[i])*8 )/ 4);
+//   }
+
+//   else{
+//     nekiNiz2.push(noviNiz[i]);
+//   }
+
+// }
+
+// console.log(nekiNiz2);
+
+let nekaMbcatrica = [
+[3,4,9]
+[13,17,43],
+[1,33,4]
+
+];
+
