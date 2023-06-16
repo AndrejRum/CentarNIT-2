2f11898e888d7e417df6b6a67141e6c3c2d6d734
Andrejj03 <andrejj.contact@gmail.com>
2023-06-16 18:02:37 +0200

    <3

---
+++ cas9/scrpipt.js
@@ -0,0 +1,17 @@
+let niz = [1,12,23,4234,121,3,15,54];
+
+let najveca = niz[0];
+
+//ako je paran uvecavamo za 2 ako je neparan umanjujemo za 1
+let noviNiz = niz.map((el) => {
+  
+  if(el % 2 ===0 ){
+    return el + 2;
+  }else{
+    return el -1 ;
+  }
+}
+)
+
+
+console.log(noviNiz);
